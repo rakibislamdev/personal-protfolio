b4fb76c287d6ff72aff47fcfb18669881cd072cb
rakibislamdev <rakibislamdev@gmail.com>
2023-03-04 00:21:23 +0600

    profile and main profile update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="keendevs">
     <!-- ======== Page title ============ -->
-    <title>Rakib - Personal Protfolio</title>
+    <title>Welcome to MD RAKIB's Web page - Md Rakib</title>
     <!-- ========== Favicon Icon ========== -->
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
     <!-- ===========  All Stylesheet ================= -->
